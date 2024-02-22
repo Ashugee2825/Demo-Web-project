91b7bedf70e8b6b46da038706a66a14304ae8f3c
Ashutosh Kumar <66332175+Ashugee2825@users.noreply.github.com>
2024-02-22 10:14:38 +0530

    Add files via upload

---
+++ DemoWebProject/src/main/java/com/demoweb/connection/DbCon.java
@@ -0,0 +1,21 @@
+package com.demoweb.connection;
+
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbCon {
+	private static Connection connection = null;
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		if(connection==null) {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_cart","root","123456");
+			System.out.println("connected");
+			
+		}
+		return connection;
+	}
+
+}
---
+++ DemoWebProject/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Shopping Cart!</title>
+<%@include file="includes/head.jsp" %>
+</head>
+<body>
+
+<h1> Hello Universe!!!</h1>
+
+
+<%@include file="includes/navbar.jsp" %>
+
+<%@page import="com.demoweb.connection.DbCon"%>
+
+<%@include file="includes/footer.jsp" %>
+ 
+</body>
+</html>
